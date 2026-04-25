9a4e688014d6e8eb90e0bc4af6d01e7e2d6deae4
RUDRADEV BERA <94984909+RDB23@users.noreply.github.com>
2026-04-26 01:21:38 +0530

    Feature/purchase 20042026 (#4)
    * Dev Fix
    
    * Dev Fix

--- inventory/src/main/java/com/starservice/inventory/inventory_app/repository/PurchaseItemRepository.java
+++ inventory/src/main/java/com/starservice/inventory/inventory_app/repository/PurchaseItemRepository.java
@@ -2,9 +2,11 @@ package com.starservice.inventory.inventory_app.repository;
 
 import com.starservice.inventory.inventory_app.entity.PurchaseItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PurchaseItemRepository extends JpaRepository<PurchaseItem, String> {
 
     List<PurchaseItem> findByPurchaseId(String purchaseId);
--- inventory/src/main/java/com/starservice/inventory/inventory_app/repository/PurchaseRepository.java
+++ inventory/src/main/java/com/starservice/inventory/inventory_app/repository/PurchaseRepository.java
@@ -2,6 +2,8 @@ package com.starservice.inventory.inventory_app.repository;
 
 import com.starservice.inventory.inventory_app.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, String> {
 }
