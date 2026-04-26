c6f01f258c69ee405262930fa82d94343b49cb30
starservice772 <starservice772@gmail.com>
2026-04-26 14:20:45 +0530

    Feature/purchase 20042026 (#6)
    * Dev Fix
    
    * Dev Fix
    
    * Dev Fix
    
    * Dev Fix
    
    ---------
    
    Co-authored-by: RDB23 <rudradev2000bera@gmail.com>

--- inventory/src/main/java/com/starservice/inventory/inventory_app/config/CorsConfig.java
+++ inventory/src/main/java/com/starservice/inventory/inventory_app/config/CorsConfig.java
@@ -19,6 +19,7 @@ public class CorsConfig {
                         .allowedHeaders("*")
                         .allowCredentials(false)
                         .maxAge(3600);
+
             }
         };
     }
