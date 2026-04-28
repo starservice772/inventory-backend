03840413beb349d160e8902ede3842f499352aed
RDB23 <rudradev2000bera@gmail.com>
2026-04-28 22:42:30 +0530

    Dev Fix

--- inventory/src/main/java/com/starservice/inventory/inventory_app/controller/PurchaseController.java
+++ inventory/src/main/java/com/starservice/inventory/inventory_app/controller/PurchaseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/purchase")
+@RequestMapping("/purchase")
 @RequiredArgsConstructor
 public class PurchaseController {
 
