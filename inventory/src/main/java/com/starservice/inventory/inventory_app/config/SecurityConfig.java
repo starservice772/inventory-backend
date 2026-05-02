5a3da9f46ebf400debc74acc34649d2aec41a7e3
RDB23 <rudradev2000bera@gmail.com>
2026-05-02 19:48:41 +0530

    Cors Fix

--- inventory/src/main/java/com/starservice/inventory/inventory_app/config/SecurityConfig.java
+++ inventory/src/main/java/com/starservice/inventory/inventory_app/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,6 +45,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(permitAll).permitAll()
                         .anyRequest().authenticated()
                 )
